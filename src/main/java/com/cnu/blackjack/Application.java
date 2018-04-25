abba5a583459a589a2baca3c7dd95ef9b3537ac3
YeonHun <yhk7357@cnu.ac.kr>
2018-04-25 19:10:38 +0900

    Yeonhun Fitst Commit

--- src/main/java/com/cnu/blackjack/Application.java
+++ src/main/java/com/cnu/blackjack/Application.java
@@ -1,4 +1,13 @@
 package com.cnu.blackjack;
 
 public class Application {
+
+
+    public static void main(String[] args) {
+        Game game = new Game(new Deck(1));
+        game.addPlayer("player1", 10000);
+        game.addPlayer("player2", 10000);
+        game.start();
+    }
+
 }
