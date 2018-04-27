4b44705806e3c2cd2d65338cd71bb0dcd9e69cfe
Park Jihoon <ampo2787@gmail.com>
2018-04-27 21:30:44 +0900

    모두 작성. 안된부분은 주석으로 달아놓음.

--- src/test/java/com/cnu/blackjack/EvaluatorTest.java
+++ src/test/java/com/cnu/blackjack/EvaluatorTest.java
@@ -8,8 +8,13 @@ public class EvaluatorTest {
     public void 게임초기화시_모든플레이어는_2장의카드를_받는다() {
         Deck deck = new Deck(1);
         Game game = new Game(deck);
-
-
+        game.addPlayer("player1",30000);
+        game.addPlayer("player2",10000);
+        game.addPlayer("player3",90000);
+        Evaluator evaluator = new Evaluator(game.getPlayerList());
+        for(String key : game.getPlayerList().keySet()) {
+            System.out.println(key + "의 카드는 " + game.getPlayerList().get(key).getHand().getCardList());
+        }
 
     }
 
@@ -30,11 +35,37 @@ public class EvaluatorTest {
 
     @Test
     public void 블랙잭이나오면_2배로_보상받고_해당_플레이어의_턴은_끝난다() {
-
+        Deck deck = new Deck(1);
+        Hand hand = new Hand(deck);
+        Player player1 = new Player(30000, hand);
+        int handValue = 0;
+        for(int i=0;i<player1.getHand().getCardList().size();i++) {
+            handValue += player1.getHand().getCardList().get(i).getRank();
+        }
+        if(handValue == 21){
+            System.out.println("블랙잭 전 자금 : " + player1.getBalance() + player1.getCurrentBet());
+            player1.setBalance(player1.getCurrentBet() * 3 + player1.getBalance());//건 돈 + 건돈 * 2 + 원래 돈
+            player1.setCurrentBet(0);
+            System.out.println("블랙잭 후 자금 : " + player1.getBalance());
+            //턴 끝난다????
+        }else{
+            System.out.println("블랙잭이 아님");
+        }
     }
 
     @Test
     public void 각_플레이어는_17이상이면_스테이한다() {
-
+        Deck deck = new Deck(1);
+        Hand hand = new Hand(deck);
+        Player player1 = new Player(30000, hand);
+        int handValue = 0;
+        for(int i=0;i<player1.getHand().getCardList().size();i++) {
+            handValue += player1.getHand().getCardList().get(i).getRank();
+        }
+        if(handValue >= 17){
+            //스테이가 그냥 아무것도 안하는거 아닌가?
+        }else{
+            player1.hitCard(); //이하면 히트
+        }
     }
 }
