e22d77cde2c2feb719646fabe616c644a2ae6194
Park Jihoon <ampo2787@gmail.com>
2018-04-27 21:09:39 +0900

    지금까지 뻘짓하고있었음. Evaluator 각_플레이어는_16이하면_히트한다 작성

--- src/main/java/com/cnu/blackjack/Application.java
+++ src/main/java/com/cnu/blackjack/Application.java
@@ -28,28 +28,37 @@ public class Application {
             System.out.println("플레이어 수 : " + PlayerName);
             System.out.println("현재 플레이어 수 : " + i);
 
-            System.out.println("현재 자금은 " +game.getPlayerList().get(PlayerName) + " 입니다.\n얼마를 배팅하시겠습니까?");
+            System.out.println("현재 자금은 " +game.getPlayerList().get(PlayerName).getBalance() + " 입니다.\n얼마를 배팅하시겠습니까?");
             game.placeBet(PlayerName, scanner.nextInt());
 
         }
         System.out.println("게임을 시작합니다.");
 
-        for(String key : game.getPlayerList().keySet()){ //Map의 key - 여기서는 플레이어네임.
-            System.out.println(key + " 님의 차례입니다.");
-            System.out.print("현재 가지고있는 카드는 ");
-            for(int i=0;i<game.getPlayerList().get(key).getHand().getCardList().size(); i++){
-                System.out.print(game.getPlayerList().get(key).getHand().getCardList().get(i));
+        while(game.getPlayerList().size() == 0) {
+            for (String key : game.getPlayerList().keySet()) { //Map의 key - 여기서는 플레이어네임.
+                System.out.println(key + " 님의 차례입니다.");
+                System.out.print("현재 가지고있는 카드는 ");
+                if (game.getPlayerList().get(key).getHand().getCardList().size() == 0) {
+                    System.out.println("없습니다. ");
+                } else {
+                    for (int i = 0; i < game.getPlayerList().get(key).getHand().getCardList().size(); i++) {
+                        System.out.print(game.getPlayerList().get(key).getHand().getCardList().get(i));
+                    }
+                }
+                System.out.println("Hit 하시겠습니까? (Y/N)");
+                scanner.nextLine();
+                String HitYN = scanner.nextLine();
+                if (HitYN == "N\n") {
+                    game.start();
+                    break;
+                } else if (HitYN == "Y\n") {
+                    game.getPlayerList().get(key).hitCard();
+                } else {
+                    System.out.println("잘못된 입력입니다.");
+                }
             }
-            System.out.println("Hit 하시겠습니까? (Y/N)");
-            if(scanner.next() == "N"){
-                break;
-            }else if(scanner.next() == "Y"){
-                game.getPlayerList().get(key).hitCard();
-            }else{
-                System.out.println("잘못된 입력입니다.");
-            }
+
         }
-        game.start();
 
     }
 
--- src/test/java/com/cnu/blackjack/EvaluatorTest.java
+++ src/test/java/com/cnu/blackjack/EvaluatorTest.java
@@ -6,13 +6,27 @@ public class EvaluatorTest {
 
     @Test
     public void 게임초기화시_모든플레이어는_2장의카드를_받는다() {
+        Deck deck = new Deck(1);
+        Game game = new Game(deck);
+
+
 
     }
 
     @Test
     public void 각_플레이어는_16이하면_히트한다() {
-
+        Deck deck = new Deck(1);
+        Hand hand = new Hand(deck);
+        Player player1 = new Player(30000, hand);
+        int handValue = 0;
+        for(int i=0;i<player1.getHand().getCardList().size();i++) {
+            handValue += player1.getHand().getCardList().get(i).getRank();
+        }
+        if(handValue <= 16){
+            player1.hitCard();
+        }
     }
+
 
     @Test
     public void 블랙잭이나오면_2배로_보상받고_해당_플레이어의_턴은_끝난다() {
